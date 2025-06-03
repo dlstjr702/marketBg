013c1c538599bac935e68a265e0c05437d50e8a3
INSEOK <dlstjr702@gmail.com>
2025-06-03 15:43:02 +0900

    파일구조생성/기본 구조 작업
    기본구조작업/인트로페이지작업

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>marketBG</title>
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="/marketBG/css/reset.css">
+    <link rel="stylesheet" href="/marketBG/css/style.css">
+
+
+
+</head>
+<body>
+    <div class="wrap">
+        <div id="shp_intro_cont">
+            <div class="shp_wrap">
+                <div class="cm_inner">
+                    <div class="shp_ir row">
+                        <!-- 이미지 영역 S -->
+                        <div class="col">
+                            <div class="img_bx">
+                                <img src="./img/main/market_intro01.png" alt="마켓이미지">
+                            </div>
+                        </div>
+                        <!-- 이미지 영역 E -->
+                        <!-- 폼 영역 S -->
+                        <div class="col">
+                            <form action="">
+                                <div class="form_cont">
+                                    <dl class="form_line">
+                                        <dt>
+                                            <p class="tit">아이디</p>
+                                        </dt>
+                                        <dd>
+                                            <div class="inp_bx">
+                                                <input type="text" name="id" placehoder="아이디를 입력해주세요.">
+                                            </div>
+                                        </dd>
+                                    </dl>
+                                    <dl class="form_line">
+                                        <dt>
+                                            <p class="tit">비밀번호</p>
+                                        </dt>
+                                        <dd>
+                                            <div class="inp_bx">
+                                                <input type="text" name="passwd" placehoder="비밀번호를 입력해주세요.">
+                                            </div>
+                                        </dd>
+                                    </dl>
+                                </div>
+                                <div class="form_info">
+                                    <div class="info_list">
+                                        <a href="#!">아이디찾기</a>
+                                        <a href="#!">비밀번호찾기</a>
+                                        <a href="#!">회원가입</a>
+                                    </div>
+                                    <button type="button" class="btn_login">로그인</button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- 폼 영역 E -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ main/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>marketBG</title>
+</head>
+<body>
+    <div class="wrap">
+        <!-- HEADER S -->
+        <header>
+            
+        </header>
+        <!-- HEADER E -->
+
+        <!-- MAIN S -->
+        <main>
+
+        </main>
+        <!-- MAIN E -->
+
+        <!-- FOOTER S -->
+        <footer>
+
+        </footer>
+        <!-- FOOTER E -->
+    </div>
+</body>
+</html>
